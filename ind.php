5ba14b4b7172bfd396fff1c93c20b7f490cf729e
ITsy <igortsy2019@gmail.com>
2019-06-26 10:10:35 +0300

    first commit

---
+++ ind.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Here must be git</h1>
+	<p>bla
+	   bla
+	   bla</p>
+</body>
+</html>
